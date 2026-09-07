058bd60d472976263a5fdb9bc41f3b8bed2e03ac
YAKSHITH-1 <yakshith388@gmail.com>
2026-09-07 14:42:54 +0530

    chore: update project dependencies and node_modules structure

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -1,11 +1,21 @@
 import express from 'express';
-
+import cors from 'cors';
+import { connectdb } from './db.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+
+app.use(cors());
 
 
 app.use(express.json());
 
+// app.get('/hep', (req, res) => {
+//     res.send({ message: "Connected" });
+// })
+
 app.listen(3000, () => {
+    connectdb();
     console.log("Server is started on port 3000");
 })
 
